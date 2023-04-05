a14968a94464bdab6a71297055ab41dde4e37f08
Lightlea <passerby535@gmail.com>
2023-04-05 14:49:22 +0800

    2023-4-5 14:50

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -7,15 +7,4 @@ module.exports = [
   { text: '关于洼地野人', link: '/message-board/' },
   { text: '豆瓣首页', link: 'https://www.douban.com/people/passerby535/?_i=9993186uNcTtu4'},
 ]
-// config.js
-module.exports = {
-  themeConfig: {
-      nav:  [
-          // 没有二级导航时可以直接添加
-         {text: '1.《空洞骑士》：神陨落的世界，虚无与荒诞的英雄', link: '/pages/f07695/'},
-         {text: '2.旧文整理（一）', link: '/pages/24768e/'},
-         {text: '3.随笔：走神', link: '/pages/cc7034/'},
-         {text: '4.一篇关于陀思妥耶夫斯基的课堂发言稿', link: '/pages/001'},
-     ]
-  }
-}
+
