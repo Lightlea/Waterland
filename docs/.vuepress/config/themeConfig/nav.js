896f03e27f79c24b7bd25b52d223683e846c3bd6
Lightlea <passerby535@gmail.com>
2023-04-06 13:57:14 +0800

    2023-4-6 13:58

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,7 +2,7 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '随笔',link: '/one/',},
-  { text: '散文',link: '/two/',},
+  { text: '散见',link: '/two/',},
   { text: '杂物间', link: '/three/' },
   { text: '关于洼地野人', link: '/message-board/' },
   { text: '豆瓣首页', link: 'https://www.douban.com/people/passerby535/?_i=9993186uNcTtu4'},
